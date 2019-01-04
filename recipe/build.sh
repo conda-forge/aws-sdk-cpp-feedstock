82b9da85300b49b3fb78d4e18cced25912bb0191
Uwe L. Korn <uwelk@xhochy.com>
2019-01-05 00:21:35 +0100

    Explicitly use host curl

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,8 @@ cmake \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DBUILD_ONLY='s3;core;transfer;config' \
   -DENABLE_TESTING=ON \
+  -DCURL_LIBRARY=${PREFIX}/lib/libcurl${SHLIB_EXT} \
+  -DCURL_INCLUDE_DIR=${PREFIX}/include \
   -DCMAKE_BUILD_TYPE=Release ..
 make -j${CPU_COUNT}
 make install
