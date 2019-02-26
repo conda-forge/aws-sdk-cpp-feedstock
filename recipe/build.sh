23cb9e33b3e3dd03e9621dc82f75dee34adec6a5
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-26 14:43:14 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@ mkdir build && cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCURL_LIBRARY=${PREFIX}/lib/libcurl${SHLIB_EXT} \
+  -DCURL_INCLUDE_DIR=${PREFIX}/include \
   -DBUILD_ONLY='s3;core;transfer;config' \
   -DENABLE_TESTING=ON \
   -DCMAKE_BUILD_TYPE=Release ..
