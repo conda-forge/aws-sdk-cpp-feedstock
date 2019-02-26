ef0dab89510fdf94c52acc1449e1528383936e5f
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-26 15:51:45 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ cmake \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCURL_LIBRARY=${PREFIX}/lib/libcurl${SHLIB_EXT} \
   -DCURL_INCLUDE_DIR=${PREFIX}/include \
-  -DOPENSSL_LIBRARIES="${PREFIX}/lib/libssl.a;${PREFIX}/lib/libcrypto.a" \
+  -DOPENSSL_LIBRARIES="${PREFIX}/lib/libssl${SHLIB_EXT};${PREFIX}/lib/libcrypto${SHLIB_EXT}" \
   -DBUILD_ONLY='s3;core;transfer;config' \
   -DENABLE_TESTING=ON \
   -DCMAKE_BUILD_TYPE=Release ..
