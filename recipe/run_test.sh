39029d3a8a5e860aeaa3c13aa47cd447db30750f
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-05-15 09:04:01 -0400

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,10 +6,10 @@ set -x
 
 if [ "$(uname)" == "Darwin" ]; then
     # OSX
-    ${CXX} test.cpp -std=c++11 -I"${PREFIX}/include" -L"${PREFIX}/lib" -Wl,-rpath,"${PREFIX}/lib" -laws-cpp-sdk-core -o test-core
+    ${CPP} -v test.cpp -std=c++11 -I"${PREFIX}/include" -L"${PREFIX}/lib" -Wl,-rpath,"${PREFIX}/lib" -laws-cpp-sdk-core -o test-core
     ./test-core
 else
     # Linux
-    ${CXX} test.cpp -std=c++11 -I"${PREFIX}/include" -L"${PREFIX}/lib" -Wl,-rpath,"${PREFIX}/lib" -laws-cpp-sdk-core -o test-core
+    ${CPP} -v test.cpp -std=c++11 -I"${PREFIX}/include" -L"${PREFIX}/lib" -Wl,-rpath,"${PREFIX}/lib" -laws-cpp-sdk-core -o test-core
     ./test-core
 fi
