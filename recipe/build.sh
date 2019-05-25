944f43e696daeef13a6fc5488b243edf989c27c9
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-05-25 10:41:30 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir build && cd build
 cmake \
+  -DCMAKE_PREFIX_PATH="${PREFIX}" \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DBUILD_ONLY='s3;core;transfer;config' \
