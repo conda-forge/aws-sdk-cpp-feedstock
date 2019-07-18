384c51c3e9944cea832735397cb4459294b97040
Isaiah Norton <isaiah.norton@gmail.com>
2019-07-18 12:19:47 -0400

    Add bld.bat for Windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,19 @@
+setlocal EnableDelayedExpansion
+
+mkdir "%SRC_DIR%"\build
+pushd "%SRC_DIR%"\build
+
+cmake -LAH -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DBUILD_ONLY="s3;core;transfer;config" ^
+      -DENABLE_UNITY_BUILD=on ^
+      -DENABLE_TESTING=off ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      ..
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
+
+popd
