7c181bb51331156a82d7223e757c4ac978aab701
Uwe L. Korn <uwe.korn@quantco.com>
2020-09-10 17:16:15 +0200

    Install dependencies from conda-forge packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,17 +8,9 @@ cmake ${CMAKE_ARGS} .. -GNinja \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DBUILD_ONLY='s3;core;transfer;config;identity-management;sts' \
-  -DENABLE_UNITY_BUILD=on \
-  -DENABLE_TESTING=off \
-  -DCMAKE_BUILD_TYPE=Release
+  -DENABLE_UNITY_BUILD=ON \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_DEPS=OFF
 
-ninja
 ninja install
-
-# Ensure that /lib64 is not created or populated
-LIB64=${PREFIX}/lib64
-if [ -d "${LIB64}" ]; then
-    echo "Error: lib64 exists!"
-    ls -lA ${LIB64}
-    exit 1
-fi
