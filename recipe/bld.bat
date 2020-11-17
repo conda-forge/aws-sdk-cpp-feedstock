24b72beb92da4c44339413a7387b5be8d4df5f04
David Brochart <david.brochart@gmail.com>
2020-11-17 02:00:38 +0100

    Build static lib too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 setlocal EnableDelayedExpansion
 
-mkdir "%SRC_DIR%"\build
-pushd "%SRC_DIR%"\build
+mkdir build-shared
+cd build-shared
 
 cmake -LAH -G "Ninja" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
@@ -18,4 +18,21 @@ if errorlevel 1 exit 1
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
 
-popd
+cd ..
+mkdir build-static
+cd build-static
+
+cmake -LAH -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DBUILD_ONLY="s3;core;transfer;config;identity-management;sts" ^
+      -DENABLE_UNITY_BUILD=on ^
+      -DENABLE_TESTING=OFF ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_DEPS=OFF ^
+      -DBUILD_SHARED_LIBS=OFF ^
+      ..
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir build && cd build
+mkdir build-shared && cd build-shared
 
 cmake ${CMAKE_ARGS} .. -GNinja \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -14,5 +14,22 @@ cmake ${CMAKE_ARGS} .. -GNinja \
   -DBUILD_DEPS=OFF \
   -DCURL_HAS_H2=ON \
   -DCURL_HAS_TLS_PROXY=ON \
+  -DBUILD_SHARED_LIBS=ON
+
+ninja install
+
+cd .. && mkdir build-static && cd build-static
+
+cmake ${CMAKE_ARGS} .. -GNinja \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DBUILD_ONLY='s3;core;transfer;config;identity-management;sts' \
+  -DENABLE_UNITY_BUILD=ON \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_DEPS=OFF \
+  -DCURL_HAS_H2=ON \
+  -DCURL_HAS_TLS_PROXY=ON \
+  -DBUILD_SHARED_LIBS=OFF
 
 ninja install
