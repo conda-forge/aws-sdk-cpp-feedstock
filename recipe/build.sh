29e63a9dd956b967630a1d9dfbe5e48c5904dc1e
Uwe L. Korn <uwe.korn@quantco.com>
2021-06-24 15:07:16 +0200

    Set CMP0075=NEW

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ cmake ${CMAKE_ARGS} .. -GNinja \
   -DCMAKE_INSTALL_LIBDIR=lib \
   -DCMAKE_MODULE_PATH="${PREFIX}/lib/cmake" \
   -DBUILD_ONLY='s3;core;transfer;config;identity-management;sts;sqs;sns;monitoring;logs' \
+  -DCMAKE_POLICY_DEFAULT_CMP0075=NEW \
   -DENABLE_UNITY_BUILD=ON \
   -DENABLE_TESTING=OFF \
   -DCMAKE_BUILD_TYPE=Release \
